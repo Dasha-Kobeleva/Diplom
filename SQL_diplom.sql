c7f62b023b7445f9e39b739460c8237a035f2467
Dasha-Kobeleva <panova1988da@gmail.com>
2024-10-13 23:27:13 +0500

    Add files via upload

---
+++ SQL_diplom.sql
@@ -0,0 +1,18 @@
+--TASK 1
+SELECT c.login, COUNT(o.id) AS "number_of_orders"
+FROM "Couriers" AS c
+LEFT JOIN "Orders" AS o ON c.id = o."courierId"
+WHERE o."inDelivery" = true
+GROUP BY c.login;
+
+
+--TASK 2
+SELECT track,
+               CASE
+                          WHEN finished = true THEN 2
+                          WHEN cancelled = true THEN -1
+                          WHEN "inDelivery" = true THEN 1
+                          ELSE 0
+               END AS status
+FROM "Orders";
+
